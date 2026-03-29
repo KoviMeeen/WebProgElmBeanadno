ca9e79805f567efa757b1bb6c6da1fda40ac9e51
Mark.K <kovacs.mark941007@gmail.com>
2026-03-29 22:34:21 +0200

    Update api.php

--- api.php
+++ api.php
@@ -6,12 +6,12 @@ header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
 $conn = new mysqli("localhost", "root", "", "adatb");
 $conn->set_charset("utf8");
 
-// A kérés metódusa (GET, POST vagy DELETE)
+
 $method = $_SERVER['REQUEST_METHOD'];
 
 switch($method) {
     case 'GET':
-        // LISTÁZÁS
+       
         $result = $conn->query("SELECT * FROM gep");
         $adatok = [];
         while($row = $result->fetch_assoc()) {
@@ -21,7 +21,7 @@ switch($method) {
         break;
 
     case 'POST':
-        // HOZZÁADÁS
+     
         $gyarto = $_POST['Gyarto'];
         $tipus = $_POST['Tipus'];
         $kijelzo = $_POST['Kijelzo'];
@@ -41,14 +41,12 @@ switch($method) {
         break;
 
     case 'DELETE':
-        // TÖRLÉS
+        
         $id = $_GET['id'];
-        // Mivel a képeden nincs 'id' mező a szerkezetnél, feltételezem, hogy a 'Gyarto' 
-        // vagy egy egyedi mező alapján törölnél. Ha van ID-d, hagyd így:
+
         $sql = "DELETE FROM gep WHERE id = $id";
         
-        // HA NINCS ID meződ, akkor pl. Típus alapján (vigyázat, több azonosat is törölhet):
-        // $sql = "DELETE FROM gep WHERE Tipus = '$id'";
+    
 
         if($conn->query($sql)) {
             echo json_encode(["status" => "torolve"]);
